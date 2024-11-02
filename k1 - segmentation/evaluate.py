55f644e02f02f48f0fb6e0c2850620b848d6dddb
0101dusica <trbovicdusica@gmail.com>
2024-11-02 09:55:57 +0100

    implemented mae calculation function

--- k1 - segmentation/evaluate.py
+++ k1 - segmentation/evaluate.py
@@ -1,7 +1,7 @@
-from sklearn.metrics import mean_absolute_error
-
-def evaluate_model(predictions, ground_truth_dict):
-    y_true = [ground_truth_dict[image_name] for image_name in predictions.keys()]
+# Evaluate the model's accuracy by comparing predicted object counts to ground truth values
+def evaluate_model(predictions, ground_truth):
+    y_true = [ground_truth[image] for image in predictions]
     y_pred = list(predictions.values())
-    mae = mean_absolute_error(y_true, y_pred)
-    return mae
+    absolute_errors = list(map(lambda y: abs(y[0] - y[1]), zip(y_true, y_pred)))
+    mean_absolute_error = sum(absolute_errors) / len(absolute_errors)
+    return mean_absolute_error
